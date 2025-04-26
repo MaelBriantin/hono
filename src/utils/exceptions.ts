7d834606c92c28c1fcac1b006d67bb4a98b190c7
MaelBriantin <mael.briantin@proton.me>
2025-04-26 22:52:23 +0200

    chore: add NotFoundException and InvalidArgumentException classes for error handling

---
+++ src/utils/exceptions.ts
@@ -0,0 +1,39 @@
+export class NotFoundException extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "NotFoundError";
+  }
+
+  static response(message?: string): Response {
+    return Response.json(
+      {
+        "error": message || "Not Found",
+      },
+      {
+        status: 404,
+        headers: {
+          "Content-Type": "application/json",
+        },
+    });
+  }
+}
+
+export class InvalidArgumentException extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "InvalidArgumentError";
+  }
+
+  static response(message?: string): Response {
+    return Response.json(
+      {
+        "error": message || "Invalid Argument",
+      },
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+    });
+  }
+}
